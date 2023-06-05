3a7c5261ac185787c3d1d98e9d29b019a0e8028b
Agustin-Placenti <agustinplacenti@hotmail.com>
2023-06-05 19:43:25 -0300

    server configuration

--- api/app.js
+++ api/app.js
@@ -1,10 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import * as http from 'http';
 
 dotenv.config();
 const app = express();
 app.use(cors());
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
 
 app.use((req, res, next) => {
   res.set("Access-Control-Allow-Credentials", "true");
@@ -21,6 +53,11 @@ app.get("/api/download-pdf", (req, res) => {
 });
 
 const port = process.env.PORT;
-app.listen(port, () => {
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+/*app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+});*/
